1897a29f0a959ffc54c1b5a63fa9f41e7e06cfd8
rmarcha06 <rmarcha06@gmail.com>
2015-10-12 12:51:05 -0500

    files added

---
+++ navController/BlueVC.swift
@@ -0,0 +1,38 @@
+//
+//  BlueVC.swift
+//  navController
+//
+//  Created by Ramiro Martinez Chavez on 12/10/15.
+//  Copyright © 2015 Ramiro Martinez Chavez. All rights reserved.
+//
+
+import UIKit
+
+class BlueVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func popToRoot(sender: AnyObject) {
+        navigationController?.popToRootViewControllerAnimated(true)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
